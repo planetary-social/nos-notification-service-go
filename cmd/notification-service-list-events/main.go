e14f821f955ffa7a91603a8e19e4023d11e4264d
boreq <boreq@0x46.net>
2023-08-24 22:18:08 +0200

    Better display

--- cmd/notification-service-list-events/main.go
+++ cmd/notification-service-list-events/main.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sort"
+	"sync"
 
 	"github.com/boreq/errors"
 	"github.com/nbd-wtf/go-nostr"
 	"github.com/planetary-social/go-notification-service/cmd/notification-service/di"
+	"github.com/planetary-social/go-notification-service/internal"
 	configadapters "github.com/planetary-social/go-notification-service/service/adapters/config"
 	"github.com/planetary-social/go-notification-service/service/domain"
+	"github.com/planetary-social/go-notification-service/service/domain/notifications"
 )
 
 func main() {
@@ -42,22 +46,24 @@ func run() error {
 	}
 	defer cleanup()
 
-	if err := listTokens(ctx, service, publicKey); err != nil {
+	tokensSet := internal.NewEmptySet[domain.APNSToken]()
+
+	if err := listTokens(ctx, service, publicKey, tokensSet); err != nil {
 		return errors.Wrap(err, "error listing APNs tokens")
 	}
 
-	if err := listEvents(ctx, service, publicKey); err != nil {
+	if err := listEvents(ctx, service, publicKey, tokensSet); err != nil {
 		return errors.Wrap(err, "error listing APNs tokens")
 	}
 
-	if err := listRelays(ctx, service, publicKey); err != nil {
-		return errors.Wrap(err, "error listing relays")
-	}
+	//if err := listRelays(ctx, service, publicKey); err != nil {
+	//	return errors.Wrap(err, "error listing relays")
+	//}
 
 	return nil
 }
 
-func listTokens(ctx context.Context, service di.Service, publicKey domain.PublicKey) error {
+func listTokens(ctx context.Context, service di.Service, publicKey domain.PublicKey, tokensSet *internal.Set[domain.APNSToken]) error {
 	fmt.Println()
 	fmt.Println("listing stored APNs tokens related to public key", publicKey.Hex())
 
@@ -71,7 +77,8 @@ func listTokens(ctx context.Context, service di.Service, publicKey domain.Public
 	}
 
 	for _, token := range tokens {
-		fmt.Println(token.Hex())
+		tokensSet.Put(token)
+		fmt.Println("token", token.Hex())
 	}
 
 	return nil
@@ -103,14 +110,24 @@ func listRelays(ctx context.Context, service di.Service, publicKey domain.Public
 	return nil
 }
 
-func listEvents(ctx context.Context, service di.Service, publicKey domain.PublicKey) error {
+type eventWithNotifications struct {
+	Event         domain.Event
+	Notifications []notifications.Notification
+}
+
+func listEvents(ctx context.Context, service di.Service, publicKey domain.PublicKey, tokensSet *internal.Set[domain.APNSToken]) error {
 	fmt.Println()
 	fmt.Println("listing stored events related to public key", publicKey.Hex())
 
 	filters, err := domain.NewFilters(nostr.Filters{
 		{
-			Authors: []string{
-				publicKey.Hex(),
+			//Authors: []string{
+			//	publicKey.Hex(),
+			//},
+			Tags: map[string][]string{
+				"p": {
+					publicKey.Hex(),
+				},
 			},
 			Limit: 100,
 		},
@@ -119,27 +136,64 @@ func listEvents(ctx context.Context, service di.Service, publicKey domain.Public
 		return errors.Wrap(err, "error creating filters")
 	}
 
+	var events []eventWithNotifications
+	var eventsLock sync.Mutex
+	var eventsWaitGroup sync.WaitGroup
+
 	for v := range service.App().Queries.GetEvents.Handle(ctx, filters) {
 		if err := v.Err(); err != nil {
 			return errors.Wrap(err, "handler returned an error")
 		}
 
 		if v.EOSE() {
-			fmt.Println("end of stored events")
 			break
 		}
 
-		evt := v.Event()
+		eventsWaitGroup.Add(1)
 
-		notifications, err := service.App().Queries.GetNotifications.Handle(ctx, v.Event().Id())
-		if err != nil {
-			return errors.Wrapf(err, "error getting notifications for event '%s'", v.Event().Id().Hex())
+		event := v.Event()
+		go func() {
+			defer eventsWaitGroup.Done()
+
+			notifications, err := service.App().Queries.GetNotifications.Handle(ctx, event.Id())
+			if err != nil {
+				fmt.Println(errors.Wrapf(err, "error getting notifications for event '%s'", event.Id().Hex()))
+				return
+			}
+
+			eventsLock.Lock()
+			defer eventsLock.Unlock()
+
+			events = append(events, eventWithNotifications{
+				Event:         event,
+				Notifications: notifications,
+			})
+		}()
+	}
+
+	eventsWaitGroup.Wait()
+
+	sort.Slice(events, func(i, j int) bool {
+		return events[i].Event.CreatedAt().Before(events[j].Event.CreatedAt())
+	})
+
+	for _, eventWithNotifications := range events {
+		evt := eventWithNotifications.Event
+
+		fmt.Println("event", evt.Id().Hex(), "type", evt.Kind().Int(), "number of tags", len(evt.Tags()), "created at", evt.CreatedAt())
+
+		if evt.PubKey() == publicKey {
+			fmt.Println("-> own event")
 		}
 
-		fmt.Println("event", evt.Id().Hex(), evt.Kind().Int())
+		//fmt.Println(string(eventWithNotifications.Event.Raw()))
 
-		for _, notification := range notifications {
-			fmt.Println("notification", notification.UUID(), "created at", notification.CreatedAt())
+		for _, notification := range eventWithNotifications.Notifications {
+			fmt.Printf("-> notification %s created at %s", notification.UUID(), notification.CreatedAt())
+			if !tokensSet.Contains(notification.APNSToken()) {
+				fmt.Print(" (for someone else)")
+			}
+			fmt.Println()
 		}
 	}
 
